3e40392f2a3fb7bb6d5bb8e1697a9ff2bf8df1e9
Swaraj <swarajchbiswas44@gmail.com>
2023-03-22 20:21:32 +0600

    second

---
+++ app/src/main/java/com/example/simpleparadox/listycity/collision.java
@@ -0,0 +1,15 @@
+package com.example.simpleparadox.listycity;
+
+public class CityTest{
+
+ private String cityid;
+
+ public String getCityid() {
+    return cityid;
+}
+
+public void setName(String cityid) {
+    this.cityid = cityid;
+}
+
+}
