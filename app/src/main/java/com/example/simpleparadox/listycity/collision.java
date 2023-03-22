d711490e1b74c28b576c2cf6d5088a21e45d0d5c
Swaraj <swarajchbiswas44@gmail.com>
2023-03-22 20:23:09 +0600

    from developer 2

---
+++ app/src/main/java/com/example/simpleparadox/listycity/collision.java
@@ -0,0 +1,15 @@
+package com.example.simpleparadox.listycity;
+
+public class CityTest{
+
+ private int cityid;
+
+ public int getCityid() {
+    return cityid;
+}
+
+public void setName(int cityid) {
+    this.cityid = cityid;
+}
+
+}
