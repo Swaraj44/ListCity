4ef1dc15872f1dfa1080b46239f867c273f510e1
Swaraj <swarajchbiswas44@gmail.com>
2023-03-22 20:38:17 +0600

    comited after collision

---
+++ app/src/main/java/com/example/simpleparadox/listycity/collision.java
@@ -0,0 +1,25 @@
+package com.example.simpleparadox.listycity;
+
+public class CityTest{
+
+<<<<<<< HEAD
+ private int cityid;
+
+ public int getCityid() {
+    return cityid;
+}
+
+public void setName(int cityid) {
+=======
+ private String cityid;
+
+ public String getCityid() {
+    return cityid;
+}
+
+public void setName(String cityid) {
+>>>>>>> master
+    this.cityid = cityid;
+}
+
+}
